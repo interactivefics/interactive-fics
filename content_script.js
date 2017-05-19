ee4b5ed5e8c167d6a07226625bd0821c4e50f0c7
Mariam Maarouf <mrf.mariam@gmail.com>
2017-05-19 18:13:06 +0200

    Remove mutationObserver

--- content_script.js
+++ content_script.js
@@ -1,15 +1,19 @@
+DISABLE_KEY = 'deactivate-this-extension-pls-interactive-fics-yalla-bina';
+
 var valChange = /\by\/n\b|\(y\/n\)|\[y\/n\]/ig;
 var person;
 var replaceAll = function (){
 	chrome.storage.local.get(null, function(items){
-		for(var key in items){
-			if(items[key]){
-				if(key=="person")
-					loadReplace(valChange, items[key]);
-				else{
-					var s = escapeRegExp(key);
-					var temp = new RegExp(s, "ig");
-					loadReplace(temp, items[key]);
+		if(items[DISABLE_KEY] !== true){
+			for(var key in items){
+				if(items[key]){
+					if(key=="person")
+						loadReplace(valChange, items[key]);
+					else if(key !== DISABLE_KEY){
+						var s = escapeRegExp(key);
+						var temp = new RegExp(s, "ig");
+						loadReplace(temp, items[key]);
+					}
 				}
 			}
 		}
@@ -22,7 +26,6 @@ function escapeRegExp(str) {
 }
 
 chrome.extension.onMessage.addListener(function(message,sender,sendResponse){
-  //This is where the stuff you want from the background page will be
   var s = escapeRegExp(message.stuff);
   var val = new RegExp(s, "ig");
   if(message.isYN)
@@ -35,8 +38,12 @@ chrome.extension.onMessage.addListener(function(message,sender,sendResponse){
 
 
 function loadReplace(rep, p){
-	if(p!=null)
-		walk(document.body, rep, p);
+	chrome.storage.local.get(DISABLE_KEY, function(obj){
+		var enabled = obj[DISABLE_KEY] !== true;
+		if(p!=null && enabled){
+			walk(document.body, rep, p);
+		}
+	});
 }
 
 function walk(node, v, p){
@@ -65,21 +72,5 @@ function handleText(textNode, val, p){
 	v = v.replace(val, p); //replaces Y/N or other value entered regardless of the case, whether it's in a bracket or not
 	textNode.nodeValue = v;
 }
-
-MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
-
-var observer = new MutationObserver(function(mutations, observer) {
-    replaceAll();
-});
-
-// define what element should be observed by the observer
-// and what types of mutations trigger the callback
-observer.observe(document, {
-  subtree: true,
-  childList: true
-  //...
-});
-
-//main
 
 replaceAll();
--- popup.js
+++ popup.js
@@ -1,28 +1,66 @@
 
+DISABLE_KEY = 'deactivate-this-extension-pls-interactive-fics-yalla-bina';
 document.addEventListener('DOMContentLoaded', function () {
+
+  chrome.storage.local.get(DISABLE_KEY, function(obj){
+  	var isDeactivated = obj[DISABLE_KEY] !== true ? false : true;
+  	var key = isDeactivated ? 'Re-activate ' : 'Deactivate ';
+  	document.getElementById('deactivate').value = key + 'Extension';
+  	if(isDeactivated){
+  		document.getElementsByTagName("BODY")[0].title = "Extension is disabled";
+  		var elements = document.getElementsByTagName("INPUT");
+	  	for(var i=0; i<elements.length; i++){
+	  		if(elements[i].id !== 'deactivate')
+	  			elements[i].disabled = 'disabled';
+	  	}
+  	}
+  });
+
+  document.getElementById('deactivate').addEventListener('click', function(){
+  	chrome.storage.local.get(DISABLE_KEY, function(obj){
+  		// 1. previous state:
+  		var wasDeactivated = obj[DISABLE_KEY] === undefined ? false : true;
+  		// 2. button:
+  		var key = wasDeactivated ? 'Deactivate ' : 'Re-activate '; 
+  		document.getElementById('deactivate').value = key + 'Extension';
+  		// 3. storage:
+  		if(wasDeactivated){
+  			chrome.storage.local.remove(DISABLE_KEY);
+  		}
+  		else{
+  			var obj = {};
+	  		obj[DISABLE_KEY] = true;
+	  		chrome.storage.local.set(obj);
+  		}
+  		chrome.tabs.reload();
+  		window.close();
+  	});
+  });
   document.getElementById('submit').addEventListener('click', clickHandler);
   document.getElementById('show-saved').addEventListener('click', function(){
 	chrome.storage.local.get(null, function(items){
 		var list = document.getElementById('theList');
 		list.innerHTML = "";
 		for(var key in items){
-			var k;
-			if(key=="person")
-				k = "Y/N";
-			else
-				k = key;
-			var v = items[key];
-			var rep = k + " -> " + v;
-			var text = document.createTextNode(rep);
-			var node = document.createElement("LI");
-			node.appendChild(text);
-			node.id = key;
-			node.className = 'one-saved-item';
-			node.addEventListener('click', function(){
-				chrome.storage.local.remove(this.id);
-				this.className+=' strikethrough';
-			});
-			list.appendChild(node);
+			if(key !== DISABLE_KEY){
+				var k;
+				if(key=="person")
+					k = "Y/N";
+				else
+					k = key;
+				var v = items[key];
+				var rep = k + " -> " + v;
+				var text = document.createTextNode(rep);
+				var node = document.createElement("LI");
+				node.appendChild(text);
+				node.id = key;
+				node.className = 'one-saved-item';
+				node.addEventListener('click', function(){
+					chrome.storage.local.remove(this.id);
+					this.className+=' strikethrough';
+				});
+				list.appendChild(node);
+			}
 		}
 	});
   });
